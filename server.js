56e97deb3257cd6fc142ef59cec4222b32f51412
Janaee <janaee.as.wallace@gmail.com>
2023-01-29 19:24:57 -0500

    added connection.js and updaed server.js file to get api to work correctly

--- server.js
+++ server.js
@@ -1,23 +1,17 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes")
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
 
+
+const PORT = process.env.port || 3001;
 const app = express();
-app.use(express.json());
-app.use("/api/users",routes);
-mongoose
-  .connect("mongodb://localhost:27017/socialnetworkDB")
-  .then(() =>
-    app.listen(
-      3001
-      //     PORT, () =>
-      //   console.log(`App listening at http://localhost:${PORT}`)
-    )
-  )
-  .then(() =>
-    console.log("Connected to Social Network DB and listening on PORT")
-  );
 
-// app.use("/api", (req, res, next) => {
-//     res.send("Test")
-// })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Connected to MongoDB and running on ${PORT}!`);
+  });
+});
